9e414509dd6ca517ebae9be2777ecd3fa9e7976f
Haseeb Majid <hello@haseebmajid.dev>
2023-07-17 21:53:10 +0100

    work

--- scripts/a.sh
+++ scripts/a.sh
@@ -6,7 +6,7 @@ tab_id_name=localhost:1212/dont_close-tmux-browser
 
 tmux display "[INFO] ello ello ello"
 bt_list=$($CURRENT_DIR/bt_list_wrapper.sh) || exit $?
-if echo "$bt_list" | grep -q "$tab_id_name$"; then
+if echo "$bt_list" | grep "$tab_id_name$"; then
 	window_id=$(echo "$bt_list" | cut -f1,3 | grep "$tab_id_name$" | cut -f1,2 -d ".")
 
 	$CURRENT_DIR/save_session.sh $current_session $window_id "$(echo "$bt_list")"
