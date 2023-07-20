c5a91675fdac5af2b64d205ddfd90af04f3a9d21
Haseeb Majid <hello@haseebmajid.dev>
2023-07-20 21:54:00 +0100

    work

--- scripts/save_and_close_browser.sh
+++ scripts/save_and_close_browser.sh
@@ -4,7 +4,7 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 current_session=$1
 tab_id_name=localhost:1212/dont_close-tmux-browser_$current_session
 
-tmux display-message -p "$TMUX_SESS"
+tmux display-message -p "HELLO $TMUX_SESS"
 bt_list=$($CURRENT_DIR/bt_list_wrapper.sh) || exit $?
 if echo "$bt_list" | grep -q "$tab_id_name$"; then
 	window_id=$(echo "$bt_list" | cut -f1,3 | grep "$tab_id_name$" | cut -f1,2 -d ".")
